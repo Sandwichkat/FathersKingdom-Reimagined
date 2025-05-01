7482822c07acf5e1e67a0aacd9030094d468e181
Sandwichkat <91664416+Sandwichkat@users.noreply.github.com>
2025-05-01 22:44:53 +0700

    another minor update

--- pack-client/update.ps1
+++ pack-client/update.ps1
@@ -4,6 +4,13 @@ $packwiz_toml_link = "https://sandwichkat.github.io/FathersKingdom-Reimagined/pa
 
 
 
+if(-Not (Get-Command "java.exe" -ErrorAction SilentlyContinue)){
+    Write-Host "Java not detected, please download." 
+    exit
+
+}
+
+
 if(-Not (Test-Path $packwiz_bootstrap_output_dir)){
     Write-Host "File not found, downloading..."
     Invoke-WebRequest -Uri $packwiz_bootstrap_link -OutFile $packwiz_bootstrap_output_dir
@@ -13,7 +20,7 @@ if(-Not (Test-Path $packwiz_bootstrap_output_dir)){
 
 if(Test-Path $packwiz_bootstrap_output_dir){
     Write-Host "Running Bootstrap..."
-    & java -jar packwiz-installer-bootstrap.jar https://sandwichkat.github.io/FathersKingdom-Reimagined/pack-client/pack.toml
+    & java -jar packwiz-installer-bootstrap.jar $packwiz_toml_link
 }
 
 
