de680d67998fa14f8a0a3389b8fc1b8aace57ac2
Sandwichkat <91664416+Sandwichkat@users.noreply.github.com>
2025-05-01 21:21:18 +0700

    Added Update Script

---
+++ pack-client/update.bat
@@ -0,0 +1,3 @@
+@echo off
+powershell -NoProfile -ExecutionPolicy Bypass -File ".\update.ps1"
+pause
---
+++ pack-client/update.ps1
@@ -0,0 +1,19 @@
+$packwiz_bootstrap_link = "https://github.com/packwiz/packwiz-installer-bootstrap/releases/download/v0.0.3/packwiz-installer-bootstrap.jar"
+$packwiz_bootstrap_output_dir = "./packwiz-installer-bootstrap.jar"
+$packwiz_toml_link = "https://sandwichkat.github.io/FathersKingdom-Reimagined/pack-client/pack.toml"
+
+
+
+if(-Not (Test-Path $packwiz_bootstrap_output_dir)){
+    Write-Host "File not found, downloading..."
+    Invoke-WebRequest -Uri $packwiz_bootstrap_link -OutFile $packwiz_bootstrap_output_dir
+} else{
+    Write-Host "Bootstrap already downloaded"
+}
+
+if(Test-Path $packwiz_bootstrap_output_dir){
+    Write-Host "Running Bootstrap..."
+    & java -jar packwiz-installer-bootstrap.jar https://sandwichkat.github.io/FathersKingdom-Reimagined/pack-client/pack.toml
+}
+
+
